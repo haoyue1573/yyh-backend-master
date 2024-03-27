dcd333a665b9ad1a7e56384ec6ee87e0eb56959c
yuhao <yuhao@qq.com>
2024-03-27 17:45:47 +0800

    -commit ---2  -- 误删service层

---
+++ src/main/java/com/yu/init/service/ChartService.java
@@ -0,0 +1,15 @@
+package com.yu.init.service;
+
+import com.yu.init.model.dto.chart.GenChartByAiRequest;
+import com.yu.init.model.entity.Chart;
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.yu.init.model.vo.BiResponse;
+
+/**
+ *
+ */
+public interface ChartService extends IService<Chart> {
+
+    BiResponse genChartData(GenChartByAiRequest genChartByAiRequest,String csvData,Long id);
+
+}
